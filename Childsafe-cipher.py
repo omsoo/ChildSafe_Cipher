08a5210a40ac45a262cb6a6104f1fe0cbef21f77
Onyebuchi Sunday Onwuajuese <onyebuchisunday0@gmail.com>
2024-04-28 02:02:58 +0100

    Added pool of graphical elements for selection

--- Childsafe-cipher.py
+++ Childsafe-cipher.py
@@ -8,3 +8,7 @@ class GraphicalPasswordInterface:
         # Header
         self.header_label = tk.Label(master, text="Create Your Graphical Password", font=("Helvetica", 16))
         self.header_label.pack(pady=10)
+
+        # Grid Layout for Graphical Elements
+        self.graphical_elements = ["🌟", "🎈", "🍕", "🚀", "🌈", "🐱", "🎨", "🐻"]  # Example graphical elements
+        self.selected_elements = []
