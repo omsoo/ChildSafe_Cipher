1d90b9d6f13fe13ae8efc2b98b078939b2cac449
Onyebuchi Sunday Onwuajuese <onyebuchisunday0@gmail.com>
2024-04-28 02:01:52 +0100

    User prompt for password creation

--- Childsafe-cipher.py
+++ Childsafe-cipher.py
@@ -4,3 +4,7 @@ class GraphicalPasswordInterface:
     def __init__(self, master):
         self.master = master
         self.master.title("Graphical Password Creation")
+
+        # Header
+        self.header_label = tk.Label(master, text="Create Your Graphical Password", font=("Helvetica", 16))
+        self.header_label.pack(pady=10)
