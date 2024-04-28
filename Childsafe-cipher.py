20d36f5ca357e44a868645868a726a1ebf6bef67
Onyebuchi Sunday Onwuajuese <onyebuchisunday0@gmail.com>
2024-04-28 02:17:14 +0100

    Add confirmation buttons

--- Childsafe-cipher.py
+++ Childsafe-cipher.py
@@ -15,5 +15,21 @@ class GraphicalPasswordInterface:
         self.grid_frame = tk.Frame(master)
         self.grid_frame.pack()
 
-        
+        for i, element in enumerate(self.graphical_elements):
+            button = tk.Button(self.grid_frame, text=element, font=("Helvetica", 20), width=4, height=2,
+                               command=lambda e=element: self.toggle_selection(e))
+            button.grid(row=i // 4, column=i % 4, padx=5, pady=5)
+
+        # Confirmation Button
+        self.confirm_button = tk.Button(master, text="Confirm Selection", font=("Helvetica", 12),
+                                        command=self.confirm_selection)
+        self.confirm_button.pack(pady=10)
+
+    def toggle_selection(self, element):
+        if element in self.selected_elements:
+            self.selected_elements.remove(element)
+        else:
+            self.selected_elements.append(element)
+
+    
         
