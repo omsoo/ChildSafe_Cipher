7bde2f2c5af04cd3d6376702dd7332f5dd4f9509
Onyebuchi Sunday Onwuajuese <onyebuchisunday0@gmail.com>
2024-04-28 02:15:22 +0100

    Add the graphical elements

--- Childsafe-cipher.py
+++ Childsafe-cipher.py
@@ -4,4 +4,12 @@ class GraphicalPasswordInterface:
     def __init__(self, master):
         self.master = master
         self.master.title("Graphical Password Creation")
+        # Header
+        self.header_label = tk.Label(master, text="Create Your Graphical Password", font=("Helvetica", 16))
+        self.header_label.pack(pady=10)
 
+        # Grid Layout for Graphical Elements
+        self.graphical_elements = ["🌟", "🎈", "🍕", "🚀", "🌈", "🐱", "🎨", "🐻"]  # Example graphical elements
+        self.selected_elements = []
+
+        
